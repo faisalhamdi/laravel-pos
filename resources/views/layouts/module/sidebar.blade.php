997b8feaae55229232e0e683fe980d1c971166a6
jambrongbro <jambrongbro17@gmail.com>
2020-01-03 14:43:45 +0700

    add feature report

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -66,6 +66,24 @@
                     </a>
                 </li>
                 @endrole
+
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa fa-shopping-bag"></i>
+                        <p>
+                            Order Management
+                            <i class="right fa fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('order.index') }}" class="nav-link">
+                                <i class="fa fa-circle-o nav-icon"></i>
+                                <p>Order</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
                 
                 @role('admin')
                 <li class="nav-item has-treeview">
